297ce7d94f4d47b0a55af132d15174ccf532b807
Martins Richard <112550418+Martcode123@users.noreply.github.com>
2022-09-21 00:05:34 +0100

    async and then

--- app.js
+++ app.js
@@ -8,7 +8,7 @@
 //     })
 // })
 
-// const emailRef = document.querySelector(".email");
+ const emailRef = document.querySelector(".email");
 
 // fetch("https://jsonplaceholder.typicode.com/users/1").then(response => {
 //     return response.json()
@@ -20,4 +20,13 @@
     
 // })
 
-// 
+// 2. Async/Await
+async function main() {
+    const response = await fetch("https://jsonplaceholder.typicode.com/users/1")
+    const data = await response.json()
+    console.log(data)
+    emailRef.innerHTML = data.email
+}
+
+main()
+
