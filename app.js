24405e275725fce316bb8297b5981470da2b9384
Martins Richard <112550418+Martcode123@users.noreply.github.com>
2022-09-20 23:48:19 +0100

    email

---
+++ app.js
@@ -0,0 +1,23 @@
+// (fetch("https://jsonplaceholder.typicode.com/users/1"))
+
+// 1. Then
+
+// fetch("https://jsonplaceholder.typicode.com/users/1").then(response => {
+//     response.json().then((data) => {
+//         console.log(data)
+//     })
+// })
+
+// const emailRef = document.querySelector(".email");
+
+// fetch("https://jsonplaceholder.typicode.com/users/1").then(response => {
+//     return response.json()
+// })
+
+// .then((data) => {
+//     console.log(data);
+//     emailRef.innerHTML = data.email
+    
+// })
+
+// 
